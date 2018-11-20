32f78392dd1bcc1c222d3becda64affc237a2c05
bohrok <nim.gnoes.eel@gmail.com>
2018-11-19 18:53:08 -0800

    Copy TicTacToe

---
+++ src/index.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2017 The boardgame.io Authors.
+ *
+ * Use of this source code is governed by a MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Client } from 'boardgame.io/react';
+import { AI } from 'boardgame.io/ai';
+import TicTacToe from './game';
+import Board from './board';
+
+const App = Client({
+    game: TicTacToe,
+    board: Board,
+    ai: AI({
+        enumerate: G => {
+            let moves = [];
+            for (let i = 0; i < 9; i++) {
+                if (G.cells[i] === null) {
+                    moves.push({ move: 'clickCell', args: [i] });
+                }
+            }
+            return moves;
+        }
+    })
+});
+
+render(<App />, document.getElementById('root'));
